2d2a5375fdd4ea610c87044b0cfdb3edcac3c44e
Marni Gross <Marni@Marnis-MBP.fios-router.home>
2017-05-24 18:16:40 -0400

    start game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,51 +4,63 @@ var words = ["dog", "cat", "long", "short"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i,", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var blank = "_";
 var guess = 10
-// var choice = randomWord()
-//function to _generate random word
-  
+var lettersGuessed = []
 
-var wordChosen = words[Math.floor(Math.random() * words.length)];
-
-console.log(wordChosen);
-
-
-
-document.onkeyup = function(event) {
-  
-var userGuess = event.key;
-
-var wordLength = wordChosen.length;
-
-var letter = wordChosen.charAt(i)
-
-
+function start(){
+	var wordChosen = words[Math.floor(Math.random() * words.length)];
+	console.log(wordChosen);
+	var wordLength = wordChosen.length;
+	//split word into array
+	var wordArray = wordChosen.split("")
+	console.log(wordArray);
 	for (var i = 0; i < wordLength; i++){
 		blank = "_ " + blank	
-	}
-	// for (var i = 0; i < guess; i++){
-	// if user guess the letter than the letter will appear
 
 	var updateBlank = "<p>" + blank + "</p>"
 
 	var gameDiv = document.getElementById("game");
 
 	gameDiv.innerHTML = updateBlank 
-
- for (var j = 0; j < guess; j++){
- 	if ( userGuess === letter){
- 		var updateLetter = "<p>" + letter + "</p>"
- 	}
- 		
- 		else{
- 			guess = guess - 1
- 		}
-
- 		
-
- }
+	
+	}
 
 }
+
+
+// .addEventListener("click", start());  
+
+// var start = function(){
+
+// 	chooseWord(words);
+	//creates blank
+	
+document.onkeyup = function(event) {
+ 
+var key = event.key; }
+
+// lettersGuessed.push(key);
+
+// console.log(lettersGuessed)
+
+
+//guess letters	 
+	
+		
+
+	
+
+
+
+
+
+
+
+
+
+
+ 
+
+
 
 
 
