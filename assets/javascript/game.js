ede2de00bed563776da94679afb8c59cbe2f9f92
Marni Gross <Marni@Marnis-MacBook-Pro.local>
2017-05-25 10:49:44 -0400

    guessing letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,15 @@
 
 var words = ["dog", "cat", "long", "short"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i,", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var blank = "_";
-var guess = 10
-var lettersGuessed = []
+var blank = "";
+var lettersGuessed = [];
+var guessedField = ""
+var lettersIn = [];
+var guessesLeft = 10
+var blankVar = []
+
+
+
 
 function start(){
 	var wordChosen = words[Math.floor(Math.random() * words.length)];
@@ -13,34 +19,114 @@ function start(){
 	//split word into array
 	var wordArray = wordChosen.split("")
 	console.log(wordArray);
+	
 	for (var i = 0; i < wordLength; i++){
-		blank = "_ " + blank	
+		blank = "_ " + blank
 
 	var updateBlank = "<p>" + blank + "</p>"
 
 	var gameDiv = document.getElementById("game");
 
-	gameDiv.innerHTML = updateBlank 
+	gameDiv.innerHTML = updateBlank;
+
 	
 	}
 
-}
+	blankVar.push(blank)
+	console.log(blankVar)
 
-
-// .addEventListener("click", start());  
-
-// var start = function(){
-
-// 	chooseWord(words);
-	//creates blank
-	
-document.onkeyup = function(event) {
+ 	document.onkeyup = function(event) {
  
-var key = event.key; }
+	var userGuess = event.key; 
 
-// lettersGuessed.push(key);
+	
 
-// console.log(lettersGuessed)
+	
+	
+		if(wordArray.indexOf(userGuess) === -1){
+			console.log("works")
+			lettersGuessed.push(userGuess);
+			console.log(lettersGuessed);
+			var afterGuessesLeft = guessesLeft - 1
+
+			var updateGuessedField = "<p>" + userGuess + "</p>"
+			var lettersDiv = document.getElementById("lettersGuessed");
+			lettersDiv.innerHTML = "Letters Guessed: " + updateGuessedField
+			
+			var updateGuessesLeft = "<p>" + afterGuessesLeft + "</p>"
+			var guessedDiv = document.getElementById("guessesLeft");
+			guessedDiv.innerHTML = "Guesses Left: " + updateGuessesLeft;
+
+	}
+		else {
+			var letterIndex = wordArray.indexOf(userGuess);
+			console.log(letterIndex)
+			
+			blankVar[letterIndex] = userGuess 
+
+			
+			console.log(blankVar);
+			
+			gameDiv.innerHTML =  blankVar
+
+		
+
+	
+
+				// console.log(letterIndex)
+				// letterReplace(letterIndex);
+			// }
+		
+
+
+			// letterReplace(wordArray)
+
+
+
+			// console.log("works")
+			// lettersIn.push(userGuess);
+			// console.log(lettersIn);
+
+		}
+		
+		}
+}
+	
+
+			
+
+			
+		
+		
+			// for(letterIndex in wordArray){
+				
+			
+
+		
+		
+
+		
+
+				
+			
+
+
+
+
+		
+
+
+
+	// if (afterGuessesLeft = 0){
+	// 	alert("You Lost")
+	
+	// }
+
+
+	
+
+
+
 
 
 //guess letters	 
